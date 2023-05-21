2b5c4c7e1778058ab9cb4986c65a2e553135f382
He-yc <1711282668@qq.com>
2023-05-22 00:32:31 +0800

    update

---
+++ HealthCodeBE/src/main/java/com/example/healthcodebe/mapper/AccountMapper.java
@@ -0,0 +1,18 @@
+package com.example.healthcodebe.mapper;
+
+import com.example.healthcodebe.entity.Account;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author hyc
+ * @since 2023-05-21 23:28:35
+ */
+@Mapper
+public interface AccountMapper extends BaseMapper<Account> {
+
+}
